e2ff08a70e9cf2ad55bea3805386d40c0fbccdb8
Anthropoid <popetyler@gmail.com>
2025-02-06 17:30:49 -0500

    Asteroids spawning and moving with collision and fail state

--- circleshape.py
+++ circleshape.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
@@ -20,3 +21,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def check_collision(self, other):
+        return self.position.distance_to(other.position) < (self.radius + other.radius)
