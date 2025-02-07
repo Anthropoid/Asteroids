549b11fa774f015342aa6fa8ee9f3171137fcec2
Anthropoid <popetyler@gmail.com>
2025-02-06 19:18:03 -0500

    Redid all files with seer stone, now shoot, but no shot collision

--- main.py
+++ main.py
@@ -1,43 +1,54 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+import sys
 import pygame
-from player import *
-from Asteroid import *
-from AsteroidField import *
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
+    shots = pygame.sprite.Group()
+
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroidfield = AsteroidField()
 
+    dt = 0
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         updatable.update(dt)
-        for entity in asteroids:
-            if entity.check_collision(player):
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
-        screen.fill((0,0,0))
-        for entity in drawable:
-            entity.draw(screen)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.get_time()/1000
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,8 @@
+import pygame
+
+def main():
+    position = pygame.Vector2(2, 4)
+    print(position)
+    return
+
+main()
